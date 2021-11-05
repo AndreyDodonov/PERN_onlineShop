720b67b4c8f1047a07f919f1845bef15c2efab46
Andrew Dodonov <nordhummer@gmail.com>
2021-11-05 20:04:27 +0300

    :rocket: add authorization

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,33 @@
 const ApiError = require('../handlers/ApiError');
-const User = require ('../models/models');
+const  {User, Basket} = require ('../models/models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 class UserController {
+    /*
+        регистрация 
+        TODO: сделать полноценную валидацию на бэке
+    */
     async registration (req, res, next) {
+        const {email, password, role} = req.body;
+        if (!email || !password) {
+            return next(ApiError.badRequest('incorrect email or password!'))
+        }
+        const candidate = await User.findOne({where: {email}});
+        if (candidate) {
+            return next(ApiError.badRequest('user with this email exists!'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
 
+        const basket = await Basket.create({userId: user.id})
+        const token = jwt.sign(
+            {id: user.id, email, role}, 
+            process.env.SECRET_KEY,
+            {expiresIn: '1h'}
+        )
+        return res.json({token})
     }
 
     async login (req, res, next) {
--- readme.md
+++ readme.md
@@ -7,3 +7,13 @@ PERN stack
 :bug: - bug fix
 
 :books: - documentation
+
+For convenience, use the better comments plugin
+
+## Usage 
+
+'''
+    npm run dev
+'''
+
+Script for running server
